d33b742cbcd65be6d5db528a6862fbcfabb676f6
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 12:03:17 +0530

    Add largest element program

---
+++ src/array/LargestElement.java
@@ -0,0 +1,20 @@
+package array;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 25, 7, 45, 18};
+
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
+
+        System.out.println("Largest Element: " + largest);
+    }
+}
