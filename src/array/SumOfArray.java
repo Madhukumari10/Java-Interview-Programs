3c88862b5b6db1a2e9b064b0e8c2f6ddc8ee1683
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 12:05:25 +0530

    Add sum of array elements program

---
+++ src/array/SumOfArray.java
@@ -0,0 +1,17 @@
+package array;
+
+public class SumOfArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+
+        System.out.println("Sum of Array Elements: " + sum);
+    }
+}
