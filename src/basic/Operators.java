f04ec883d624f10b10325507f37e50083f5b71c6
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 11:49:07 +0530

    Add operators example

---
+++ src/basic/Operators.java
@@ -0,0 +1,30 @@
+package basic;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+
+        // Logical Operators
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+    }
+}
