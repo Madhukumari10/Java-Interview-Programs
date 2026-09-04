9a08de14fc984945b9f7406d70413c52fdf68cab
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 12:18:15 +0530

    Add abstraction example

---
+++ src/oops/AbstractionExample.java
@@ -0,0 +1,29 @@
+package oops;
+
+abstract class Vehicle {
+
+    abstract void start();
+
+    void stop() {
+        System.out.println("Vehicle stopped");
+    }
+}
+
+class Car extends Vehicle {
+
+    @Override
+    void start() {
+        System.out.println("Car started");
+    }
+}
+
+public class AbstractionExample {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Car();
+
+        vehicle.start();
+        vehicle.stop();
+    }
+}
