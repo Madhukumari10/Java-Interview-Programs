c6a45b61145a5569f6f793e88d533b2eb642e9e6
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 12:42:12 +0530

    Add array element search program

---
+++ src/array/SearchElement.java
@@ -0,0 +1,26 @@
+package array;
+
+public class SearchElement {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int searchElement = 30;
+        boolean found = false;
+
+        for (int number : numbers) {
+
+            if (number == searchElement) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println(searchElement + " is present in the array.");
+        } else {
+            System.out.println(searchElement + " is not present in the array.");
+        }
+    }
+}
