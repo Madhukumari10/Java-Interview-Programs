f9f9b187801676ce943f3e1a16fddd487b810f6a
Madhu Kumari <143492087+Madhukumari10@users.noreply.github.com>
2026-09-04 11:47:39 +0530

    Add variables and data types example

---
+++ src/basic/VariablesAndDataTypes.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 28;
+        double salary = 45000.50;
+        char grade = 'A';
+        boolean isJavaDeveloper = true;
+        String name = "Madhu";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java Developer: " + isJavaDeveloper);
+    }
+}
